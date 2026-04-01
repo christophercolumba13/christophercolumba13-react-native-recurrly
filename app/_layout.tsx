dea314e4de48bd37bad3503643daa383747bbb21
Christopher <christophercolumba13@users.noreply.github.com>
2026-04-01 02:35:40 -0500

    feat: preoject setup, nativewind, routing, tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
-import { Stack, Redirect } from "expo-router";
+import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
     return (
         <Stack screenOptions={{ headerShown: false }}>
-            {/* Esto redirige automáticamente al home de tabs */}
-            <Stack.Screen name="(tabs)/index" />
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="(auth)" />
+            <Stack.Screen name="onboarding" />
         </Stack>
     );
 }
